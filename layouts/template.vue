d07d328f56c3d62fa92decbf1909ba0fdf6cbc35
kiminkyeoung <minto43@hanmail.net>
2021-03-01 16:56:29 +0900

    [UPDATE] 인터렉션 하기전 프로젝트 정리

--- layouts/template.vue
+++ layouts/template.vue
@@ -3,16 +3,3 @@
         <nuxt/>
     </div>
 </template>
-<script>
-    import Header from '~/components/Header.vue'
-    import Nav from '~/components/Nav.vue'
-    import Footer from '~/components/Footer.vue'
-
-    export default {
-        components: {
-            Header,
-            Nav,
-            Footer
-        }
-    }
-</script>
