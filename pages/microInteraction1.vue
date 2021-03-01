4f1ea69b9893c5ae1e4d38747ce7410416135308
kiminkyeoung <minto43@hanmail.net>
2021-03-01 20:33:17 +0900

    [UPDATE] 알람, 버튼 인터랙션 코드

--- pages/microInteraction1.vue
+++ pages/microInteraction1.vue
@@ -1,7 +1,99 @@
 <template>
-    <div>
+    <div class="wrapper">
+        <a :class="alarmEventClass" @click="alarmEvent">
+            <img src="~assets/images/alarm.svg" />
+            <span>1</span>
+        </a>
     </div>
 </template>
+<script>
+    export default {
+        data() {
+            return{
+                alarmEventClass:'',
+            }
+        },
+        methods: {
+            alarmEvent() {
+                console.log(this.alarmEventClass);
+                if( this.alarmEventClass  === 'alarm-shake') {
+                    this.alarmEventClass = '';
+                } else {
+
+                    this.alarmEventClass = 'alarm-shake';
+                }
+
+            }
+        }
+    }
+</script>
 <style>
+    @import url('https://fonts.googleapis.com/css2?family=Staatliches&display=swap');
+    .wrapper {
+        padding:50px;
+        text-align:center;
+    }
+    @keyframes alarm-shake {
+        20%,60% {
+            transform: rotate(20deg);
+        }
+
+        40%,80% {
+            transform: rotate(-20deg);
+        }
+        100% {
+            transform: rotate(0deg);
+        }
+    }
+    .alarm-shake {
+        animation-name: alarm-shake;
+        animation-duration: .5s;
+        animation-iteration-count:2;
+        animation-fill-mode: both;
+    }
+    a {
+        display:block;
+        position:relative;
+        width:200px;
+        height:200px;
+        margin:0 auto;
+    }
+    a img {width:200px;}
+    @keyframes number-alarm {
+        0% {
+            transform: scale(0);
+        }
+
+        50% {
+            transform: scale(1.1);
+        }
+
+        100% {
+            transform: scale(1);
+        }
+    }
+    .alarm-shake span {
+        animation-name: number-alarm;
+        animation-duration: 1s;
+        animation-delay: 1s;
+        animation-fill-mode: both;
+    }
+    span {
+        position:absolute;
+        top:15px;
+        right:15px;
+        display:block;
+        width:60px;
+        height:60px;
+        line-height:60px;
+        background:#EE6B60;
+        color:#fff;
+        border-radius:50px;
+        font-size:26px;
+        font-weight:bold;
+        text-align:center;
+        font-family: 'Staatliches', cursive;
+        transform: scale(0);
+    }
 </style>
 
---
+++ pages/microInteraction2.vue
@@ -0,0 +1,134 @@
+<template>
+    <div class="wrapper">
+        <a :class="state + ' button'" @click="submit">
+            <div class="button-wrap">
+                <div>SUBMIT</div>
+                <div class="loading-div">
+                    <span></span>LOADING
+                </div>
+                <div class="success-div">
+                    <svg height="24" viewBox="0 0 24 24" width="24">
+                        <path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"/>
+                    </svg>
+                    SUCCESS
+                </div>
+            </div>
+        </a>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                state: 'submit',
+            }
+        },
+        methods: {
+            submit() {
+                this.state = "loading";
+                setTimeout(() => {
+                    this.state = "success";
+                    console.log("bbb")
+                }, 3000);
+            }
+        }
+    }
+</script>
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Staatliches&display=swap');
+
+    .wrapper {
+        padding: 50px;
+        text-align: center;
+    }
+
+    a.button {
+        display: block;
+        position: relative;
+        width: 200px;
+        height: 50px;
+        margin: 0 auto;
+        font-family: 'Staatliches', cursive;
+        font-size: 26px;
+        border-radius: 10px;
+        color: #fff;
+        background: #00A178;
+        font-weight: 400;
+        letter-spacing: 1px;
+        overflow: hidden;
+        cursor: pointer;
+        transition:all ease-in .5s;
+    }
+
+    .button-wrap {
+        transition:all ease-in-out .2s;
+    }
+    .loading .button-wrap {
+        transform: translateY(-50px);
+    }
+    .loading.button {
+        background:#22A6C0;
+    }
+    .success .button-wrap {
+        transform: translateY(-100px);
+    }
+
+    .success.button {
+        background:#4CC290;
+    }
+    div.button-wrap div {
+        height: 50px;
+        line-height: 50px;
+    }
+
+    @keyframes loading {
+        0% {
+            transform: rotate(0deg);
+        }
+        100% {
+            transform: rotate(360deg);
+        }
+    }
+
+    .loading-div span {
+        display: inline-block;
+        width: 20px;
+        height: 20px;
+        border-radius: 50%;
+        border: 2px solid #CCDACD;
+        border-top: 3px solid #2C656B;
+        margin: 0 10px 0 0;
+        animation: loading 1s ease-in-out infinite;
+    }
+
+    @keyframes success {
+        0% {
+            stroke-dashoffset: -50px;
+        }
+        70% {
+            stroke-dashoffset: 0;
+            transform: scale(1);
+        }
+        80% {
+            transform: scale(1.3);
+        }
+        95% {
+            transform: scale(1);
+        }
+        100% {
+            transform: scale(1);
+        }
+    }
+
+    .success svg {
+        fill: transparent;
+        stroke: #2C656B;
+        stroke-width: 3px;
+        stroke-dasharray: 50px;
+        margin: -5px 2px 0 0;
+    }
+    .success .success-div svg {
+        animation: success 2s ease-in-out;
+    }
+</style>
+
