7ee8fc4c6c640216b2fc5831cd9b349f6b9fc6d8
kiminkyeoung <minto43@hanmail.net>
2021-06-05 21:51:56 +0900

    [UPDATE] 데이트피커 deug 기능

---
+++ pages/datepicker.vue
@@ -0,0 +1,21 @@
+<template>
+    <div style="padding:50px; position:relative;">
+        <date-picker v-model="date" :config="options"
+        style="width:200px;"/>
+    </div>
+</template>
+<script>
+export default {
+        data() {
+            return {
+                date: new Date(),
+                options: {
+                    format: 'YYYY-MM-DD',
+                    debug:true,
+                }
+            }
+
+        },
+    }
+</script>
+
--- plugins/vue-client.js
+++ plugins/vue-client.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
-/*import printJS from 'print-js'
-Vue.use(printJS);*/
 
 
+import 'bootstrap/dist/css/bootstrap.css';
 import datePicker from 'vue-bootstrap-datetimepicker';
 import 'pc-bootstrap4-datetimepicker/build/css/bootstrap-datetimepicker.css';
+Vue.use(datePicker);
+
 
 import Vuebar from 'vuebar';
 Vue.use(Vuebar);
@@ -13,10 +14,13 @@ import VueNumber from 'vue-number-animation'
 Vue.use(VueNumber);
 
 import VueApexCharts from 'vue-apexcharts'
-Vue.use(VueApexCharts)
-Vue.component('apexchart', VueApexCharts)
+Vue.use(VueApexCharts);
+Vue.component('apexchart', VueApexCharts);
 
 import SimpleVueValidation from 'simple-vue-validator';
 Vue.use(SimpleVueValidation);
 
-Vue.use(datePicker);
+
+
+/*import printJS from 'print-js'
+Vue.use(printJS);*/
